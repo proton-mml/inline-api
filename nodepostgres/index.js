3ad031fe3711a5257321ef204419a811964d5895
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-06-25 13:22:57 -0300

    Novos testes no nodepostgres para empresa e clientecadastrado

--- nodepostgres/index.js
+++ nodepostgres/index.js
@@ -23,9 +23,22 @@ async function test() {
 
     av_fanfafa = (await Avaliacao.getByEmailEstabelecimento('fanfafa@fanfa.com'))
     console.log(av_fanfafa);
-    
+
+    var x = await ClienteCadastrado.insert("Josasos", "joshss@j.com", "2222-2222", "-", "senha");
+    console.log("Inserindo ClienteCadastrado>", x);
+    var y = await ClienteCadastrado.getByEmail("joshss@j.com");
+    console.log("Selecionando ClienteCadastrado>", y);
+    var z = await ClienteCadastrado.deleteByEmail("joshss@j.com");
+    console.log("Deletando ClienteCadastrado>", z);
+
+    var x = await Empresa.insert("MC", "MC@j.com", "00000", ['sp', 'sp', 'rua jaca', '100', 'ap 2'], "senha");
+    console.log("Inserindo Empresa>", x);
+    var y = await Empresa.getByEmail("MC@j.com");
+    console.log("Selecionando Empresa>", y);
+    var z = await Empresa.deleteByEmail("MC@j.com");
+    console.log("Deletando Empresa>", z);
+
     PGConnection.end();
 }
 
 test();
-
