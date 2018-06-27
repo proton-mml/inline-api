f5679d2458c0931597efac4e3121aba3ab46383c
marck <marcos.vinicius.wz@gmail.com>
2018-06-26 22:47:18 -0300

    return user

--- controllers/lib/Authorize.js
+++ controllers/lib/Authorize.js
@@ -8,6 +8,7 @@ export default class Authorize {
     if (resp) {
       var isValid = EncryptionUtility.compare(senha, resp.senha);
       if (isValid) return ({
+        user:resp,
         token: EncryptionUtility.generateToken(resp, 'frangos'),
         success: true
       });
@@ -20,18 +21,5 @@ export default class Authorize {
       error: "email nao encontrado",
       success: false
     });
-  }
-  static async newUser(body) {
-    let usuario = "SELECT * FROM inline.usuario WHERE usuario.email = $1";
-    var resp = (await PGConnection.query(usuario, [body.email])).rows[0];
-    if (resp) {
-      return ({
-        error: "email em uso",
-        success: false
-      });
-    }
-    else {
-
-    }
   }
 }
