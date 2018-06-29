714d2d831183820d6a9ead9d394132af573fa1a4
Matheus Tavares <matheusmalavasi@uol.com.br>
2018-06-28 23:26:18 -0300

    update filas

--- db/schemas/filas_schema.js
+++ db/schemas/filas_schema.js
@@ -1,44 +1,44 @@
 export function filas(Schema) {
 	return new Schema({
-	  id_estabelecimento: String,
-	  id_fila: String,
-    data_hora_inicio: String,
-    data_hora_fim: String,
-    tamanho: Number,
-    filas_cronologicas:[{
-		  aceita_premium: Boolean,
-		  aceita_preferencial: Boolean,
-	    entradas:[{
-			  id_usuario: String,
-			  distancia: String,
-		    data_hora_entrada: String,
-		    preferencial: Boolean,
-		    premium: Boolean,
-		    posicao: Number
-			}],
-	    concluidos:[{
-			  id_usuario: String,
-			  distancia: String,
-		    data_hora_entrada: String,
-		    preferencial: Boolean,
-		    premium: Boolean,
-		    posicao: Number,
-		    data_hora_saida: String,
-		    desistencia_ou_atendido: String
-			}],
-		}],
-    filas_agendadas: [{
-	    agendamentos:[{
-			  id_usuario: String,
-		    data_hora_agendada: String,
-		    data_hora_criacao: String
-			}],
-	    agendamentos_concluidos:[{
-			  id_usuario: String,
-		    data_hora_agendada: String,
-		    data_hora_criacao: String,
-		    desistencia_ou_atendido: String
-			}],
-		}]
+		id_estabelecimento: String,
+		id_fila: String,
+		data_hora_inicio: String,
+		data_hora_fim: String,
+		tamanho: Number,
+		cronologica: {
+			  aceita_premium: Boolean,
+			  aceita_preferencial: Boolean,
+		    entradas:{
+				id_usuario: String,
+				distancia: String,
+			    data_hora_entrada: String,
+			    preferencial: Boolean,
+			    premium: Boolean,
+			    posicao: Number
+				}],
+		    concluidos:[{
+				id_usuario: String,
+				distancia: String,
+			    data_hora_entrada: String,
+			    preferencial: Boolean,
+			    premium: Boolean,
+			    posicao: Number,
+			    data_hora_saida: String,
+			    desistencia_ou_atendido: String
+				}],
+			},
+		agendada: {
+		    agendamentos:[{
+				 id_usuario: String,
+			    data_hora_agendada: String,
+			    data_hora_criacao: String
+				}],
+		    agendamentos_concluidos:[{
+				  id_usuario: String,
+			    data_hora_agendada: String,
+			    data_hora_criacao: String,
+			    desistencia_ou_atendido: String
+				}],
+			}
 	}, { timestamps	: { updatedAt: 'updated_at', createdAt: 'created_at' } });
 }
